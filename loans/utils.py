05586fc5ba387129076c24ea77f6151ae2b93d88
dalienst <oduordalienst@gmail.com>
2025-09-09 10:29:13 +0300

    add loan app

--- loans/utils.py
+++ loans/utils.py
@@ -3,10 +3,8 @@ import secrets
 from datetime import datetime
 
 
-def generate_account_number():
-    """Generate a random 10-digit account number."""
+def generate_loan_account_number():
+    """Generate a random 10-digit loan account number."""
     year = datetime.now().year % 100
     random_number = "".join(secrets.choice(string.digits) for _ in range(8))
-    return f"{year}{random_number}"
-
-
+    return f"LN{year}{random_number}"
