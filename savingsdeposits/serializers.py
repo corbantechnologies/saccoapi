01e019b9db19f457a4226b9d066d4da246c3e3e6
Dalienst Owino <oduordalienst@gmail.com>
2025-09-21 04:41:21 +0300

    set up savings

---
+++ savingsdeposits/serializers.py
@@ -0,0 +1,31 @@
+from rest_framework import serializers
+
+from savingsdeposits.models import SavingsDeposit
+from savings.models import SavingsAccount
+
+
+class SavingsDepositSerializer(serializers.ModelSerializer):
+    savings_account = serializers.SlugRelatedField(
+        slug_field="account_number", queryset=SavingsAccount.objects.all()
+    )
+    deposited_by = serializers.CharField(source="deposited_by.username", read_only=True)
+    is_active = serializers.BooleanField(default=True)
+
+    class Meta:
+        model = SavingsDeposit
+        fields = [
+            "savings_account",
+            "deposited_by",
+            "amount",
+            "phone_number",
+            "description",
+            "currency",
+            "payment_method",
+            "deposit_type",
+            "transaction_status",
+            "is_active",
+            "receipt_number",
+            "created_at",
+            "updated_at",
+            "reference",
+        ]
