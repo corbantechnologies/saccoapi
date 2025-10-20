c018572559923591a56ec83322826fd4b40bf927
Dalienst Owino <oduordalienst@gmail.com>
2025-10-21 00:00:26 +0300

    add savings withdrawal admin

--- savingswithdrawals/admin.py
+++ savingswithdrawals/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
-# Register your models here.
+from savingswithdrawals.models import SavingsWithdrawal
+
+
+class SavingsWithdrawalAdmin(admin.ModelAdmin):
+    list_display = (
+        "withdrawn_by",
+        "amount",
+        "savings_account",
+        "created_at",
+        "transaction_status",
+    )
+    search_fields = ("withdrawn_by__member_no", "savings_account__account_number")
+    list_filter = ("created_at", "transaction_status")
+    ordering = ["-created_at"]
+
+
+admin.site.register(SavingsWithdrawal, SavingsWithdrawalAdmin)
