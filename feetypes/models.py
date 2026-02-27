3343db95b89f970d629c9b98142aed095af58723
Dalienst <oduordalienst@gmail.com>
2026-02-27 16:16:00 +0300

    add key fee type fields

--- feetypes/models.py
+++ feetypes/models.py
@@ -12,7 +12,12 @@ class FeeType(UniversalIdModel, TimeStampedModel, ReferenceModel):
     name = models.CharField(max_length=2550, unique=True)
     description = models.TextField(blank=True, null=True)
     standard_amount = models.DecimalField(max_digits=12, decimal_places=2, default=0.00)
+    # For the GL accounts
     is_income = models.BooleanField(default=False)
+    is_expense = models.BooleanField(default=False)
+    is_liability = models.BooleanField(default=False)
+    is_asset = models.BooleanField(default=False)
+    is_equity = models.BooleanField(default=False)
     is_active = models.BooleanField(default=True)
 
     class Meta:
